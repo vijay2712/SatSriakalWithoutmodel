93e77154d567f53bed81c5a88949427f3aca67af
MalikaJain09 <48240556+MalikaJain09@users.noreply.github.com>
2020-12-25 12:37:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -93,4 +93,6 @@ def image_stitch():
 
 
 if __name__=="__main__":
-    app.run(debug = True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+#     app.run(debug = True)
